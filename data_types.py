bc9930231b4135af7b3a3c8602f14c5e6287b5a2
2212072 <2212072@slu.edu.ph>
2024-08-07 20:24:45 +0800

    Exercise 2 - 4

---
+++ data_types.py
@@ -0,0 +1,27 @@
+# Exercise 2
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 3
+
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 4
+myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 5
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue))) 
